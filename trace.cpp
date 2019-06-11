7412547ae2c6113d5373adc85f555ba288044f21
Harsha Sharma <harshasharmaiitr@gmail.com>
2019-06-11 16:03:45 +0800

    check satisfiability of all traces

--- trace.cpp
+++ trace.cpp
@@ -27,15 +27,15 @@ vector<struct tracenode*> trace::leaf_nodes(struct tracenode *node, vector<struc
 
 vector<vector<struct tracenode*>> trace::get_paths(struct tracenode *node, vector<vector<struct tracenode*>> paths, vector<struct tracenode *> path) {
   //vector<struct tracenode* > path;
-  cout << "in get paths " << endl;
+  //cout << "in get paths " << endl;
   if (node == NULL) {
     return paths;
   }
 
-  cout << "adding node in path " << endl;
+  //cout << "adding node in path " << endl;
   path.push_back(node);
   if (node->left == NULL && node->right == NULL) {
-    cout << "path length in get_path " << path.size() << endl;
+    //cout << "path length in get_path " << path.size() << endl;
     paths.push_back(path);
     path.clear();
   }
@@ -43,7 +43,7 @@ vector<vector<struct tracenode*>> trace::get_paths(struct tracenode *node, vecto
     paths = get_paths(node->left, paths, path);
   if (node->right != NULL)
     paths = get_paths(node->right, paths, path);
-  cout << "no of paths in  get_paths " << paths.size() << endl;
+  //cout << "no of paths in  get_paths " << paths.size() << endl;
   return paths;
 }
 
@@ -56,13 +56,13 @@ void trace::print_all_paths() {
   for (it = paths.begin(); it != paths.end(); it++) {
     cout << "Trace " << i++ << " : " << endl;
     vector <struct tracenode *> path = (*it);
-    cout << "path len " << path.size() << endl;
+    //cout << "path len " << path.size() << endl;
     vector <struct tracenode *>::iterator it2;
     for (it2 = path.begin(); it2 != path.end(); it2++) {
       if ((*it2)->decl) {
 	cout << "Allocate(" << (*it2)->a << ")" << endl;
-      } else if ((*it2)->op == "==") {
-	cout << "Assign(" << (*it2)->a << ", " << (*it2)->value << ")" << endl;
+      //} else if ((*it2)->op == "==") {
+	//cout << "Assign(" << (*it2)->a << ", " << (*it2)->value << ")" << endl;
       } else {
 	cout << "Assert(" << (*it2)->a << " " << (*it2)->op << " " << (*it2)->value << ")" << endl;
       }
@@ -123,9 +123,10 @@ void trace::add_ite_node(struct tracenode *t1, struct tracenode *t2, struct trac
   temp3->expr = e3;
   temp3->decl = false;*/
   //cout << "in add ite node " << endl;
+  //cout << "ite node condition I " << t1->a << " " << t1->op << " " << t1->value << endl;
   vector<struct tracenode*> leaves;
   leaves = trace::leaf_nodes(root, leaves);
-  cout << "leaf nodes in ite " << leaves.size() << endl;
+  //cout << "leaf nodes in ite " << leaves.size() << endl;
   vector<struct tracenode*>::iterator it;
   for (it = leaves.begin(); it != leaves.end(); it++) {
     (*it)->left = t1;
@@ -281,10 +282,10 @@ trace::trace() {
   trace::add_allocate_in("L3+16", 16);
   trace::add_allocate_in("L3+4", 4);
   trace::add_allocate_in("L3+80", 16);
-  trace::add_allocate_in("L3+32", 16);
+  trace::add_allocate_in("L3+32", 16);*/
   trace::add_allocate_in("L4+0", 16);
   trace::add_assert_in("L4+0", ">=", 0);
-  trace::add_assert_in("L4+0", "<=", 65536);
+  trace::add_assert_in("L4+0", "<=", 65536);/*
   trace::add_allocate_in("L4+16", 16);
   trace::add_assert_in("L4+16", ">=", 0);
   trace::add_assert_in("L4+16", "<=", 65536);
@@ -313,16 +314,57 @@ trace::trace() {
 }
 
 int trace::execute() {
-  /*
-  solver s(ctx);
-  vector<expr>::iterator it;
-  for (it = trace::Exprs.begin(); it != trace::Exprs.end(); it++) {
-    s.add((*it));
+  vector<vector<struct tracenode* >> paths;
+  vector<struct tracenode*> path;
+  paths = trace::get_paths(root, paths, path);
+  vector<vector<struct tracenode* >>::iterator it;
+  int path_i = 1;
+  for (it = paths.begin(); it != paths.end(); it++) {
+    cout << "Trace " << path_i++ << " ";
+    solver s(ctx);
+    vector<expr> decl_exprs;
+    vector<struct tracenode*> t_path = (*it);
+    vector<struct tracenode*>::iterator it2;
+    for (it2 = t_path.begin(); it2 != t_path.end(); it2++) {
+      if((*it2)->decl) {
+	string unique_name = (*it2)->a;
+	const char *name = unique_name.c_str();
+	Z3_sort int_sort = Z3_mk_int_sort(ctx);
+	Z3_symbol s = Z3_mk_string_symbol(ctx, const_cast<char *>(name));
+	Z3_ast x = Z3_mk_const(ctx, s, int_sort);
+	expr eq(ctx, x);
+	decl_exprs.push_back(eq);
+      } else {
+	vector<expr>::iterator decl_it;
+	for (decl_it = decl_exprs.begin(); decl_it != decl_exprs.end(); decl_it++) {
+	  if ((*decl_it).decl().name().str() == (*it2)->a) {
+	    if ((*it2)->op == ">=") {
+	      expr eq2 = (*decl_it) >=  (*it2)->value;
+	      s.add(eq2);
+	    } else if ((*it2)->op == ">") {
+	      expr eq2 = (*decl_it) > (*it2)->value;
+	      s.add(eq2);
+	    } else if ((*it2)->op == "<") {
+	      expr eq2 = (*decl_it) < (*it2)->value;
+	      s.add(eq2);
+	    } else if ((*it2)->op == "<=") {
+	      expr eq2 = (*decl_it) <= (*it2)->value;
+	      s.add(eq2);
+	    } else if ((*it2)->op == "==") {
+	      expr eq2 = (*decl_it) == (*it2)->value;
+	      s.add(eq2);
+	    } else if ((*it2)->op == "!=") {
+	      expr eq2 = (*decl_it) != (*it2)->value;
+	      s.add(eq2);
+	    }
+	  }
+	}
+      }
+    }
+    cout << s.check() << endl;
   }
-  cout << s.check() << endl;
-  model m = s.get_model();
-  cout << "Model:\n" << m << "\n";*/
 }
+
 
   /*vector<struct allocate_in*>::iterator it_alloc;
   for (it_alloc = allocate_ins.begin(); it_alloc != allocate_ins.end(); it_alloc++) {
